db2afcbc79b9b3d06fdfcfe8e5f12b9bd78ad642
AndresQ <andresdquiroga@yahoo.com>
2024-07-02 01:19:50 -0300

    Views are adapted to inherintance from base...

--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-def helloworld(request):
-    return HttpResponse('<h1>Hello World</h1>')
+def index(request):
+    return render(request, 'index.html', {
+        'banner_image': 'img/banner_home.jpg', 
+        'banner_text': 'Vintage & Deco',})
+
+def products(request):
+    return render(request, 'products.html', {'banner_image': 'img/banner_products.jpg', 'banner_text': 'Nuestros productos'})
+
+def sales(request):
+    return render(request, 'sales.html', {'banner_image': 'img/banner_sale.jpeg', 'banner_text': ''})
+
+def about(request):
+    return render(request, 'about.html', {'banner_image': 'img/banner_about.jpg', 'banner_text': 'Sobre nosotros'})
+
+def contact(request):
+    return render(request, 'contact.html', {'banner_image': 'img/banner_contact.jpg', 'banner_text': 'Contacto'})
