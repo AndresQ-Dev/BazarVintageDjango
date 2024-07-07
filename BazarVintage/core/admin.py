a574ef22a032e95dc8f884c06a4e032545781907
AndresQ <andresdquiroga@yahoo.com>
2024-07-07 18:49:16 -0300

    add new style to admin with Django Jazzmin...

--- BazarVintage/core/admin.py
+++ BazarVintage/core/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.db.models import Q
 
 # Register your models here.
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'stock', 'category', 'sale', 'image_tag') 
+    list_display = ('name', 'description', 'price', 'stock', 'category', 'sale', 'image_tag') #agrego columnas a mostrar 
+    search_fields = ['name',] #campo por el cual realiza la búsqueda
 
-    def image_tag(self, obj):
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+
+        # Modificar la busqueda para que sea case insensitive
+        search_term_normalized = search_term.lower()  # Convierto a minúsculas
+        queryset |= self.model.objects.filter(
+            Q(name__icontains=search_term_normalized)
+        )
+
+        return queryset, use_distinct
+    
+    def image_tag(self, obj):# Para mostrar la imágen en vez del URL de la imágen...
         if obj.image:
             return format_html('<img src="{}" style="max-height: 100px;"/>', obj.image.url)
         return "No Image"
