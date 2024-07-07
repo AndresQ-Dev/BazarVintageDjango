9425ee9b582a88e2ba6ba10c9f82c3e1a829835f
AndresQ <andresdquiroga@yahoo.com>
2024-07-07 16:28:57 -0300

    add image column to the products list...

--- BazarVintage/core/admin.py
+++ BazarVintage/core/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'stock', 'category', 'sale', 'image_tag') 
+
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 100px;"/>', obj.image.url)
+        return "No Image"
+
+    image_tag.short_description = 'Image'
+    
+admin.site.register(Product, ProductAdmin)
